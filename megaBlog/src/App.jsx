477326f1c75bb78bfba013087e8140b77eef7aef
Tanishq Sharma <123290269+Tanishq-0308@users.noreply.github.com>
2024-11-07 10:02:10 +0000

    createApp

---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Hello Blog....</h1>
+      <p>welcome to new blog</p>
+    </>
+  )
+}
+
+export default App
